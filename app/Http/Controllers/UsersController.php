105b5541b732b6ee264ddc6f1ba8599592353e25
Edoardo Di Giuseppe <edoardodg09@gmail.com>
2025-05-21 11:15:02 +0200

    Initial commit

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,25 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Request;
+
+class UsersController extends Controller{
+
+    public function index(){
+        //ritorna tutti gli users
+    }
+    public function create(){
+         //crea view
+    }
+    public function store(Request $request){
+
+    }
+    public function edit($id){
+
+    }
+    public function update(Request $request, $id){
+
+    }
+    public function destroy($id){
+
+    }
+}
